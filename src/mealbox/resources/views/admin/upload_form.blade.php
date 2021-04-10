9917485fb90e4be08590444f7adec79ea1bb4144
Danke1732 <s0lfa1732@gmail.com>
2021-04-10 17:59:36 +0900

    商品掲載用form等Viewファイル作成

---
+++ src/mealbox/resources/views/admin/upload_form.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>商品登録フォーム</title>
+  <!-- Scripts -->
+  <script src="{{ asset('js/app.js') }}" defer></script>
+  <!-- Styles -->
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/form-validation.css') }}" rel="stylesheet">
+</head>
+<body>
+  @include('shared/header')
+  <div class="container" style="margin-top: 110px;">
+  <div class="py-5 text-center">
+    
+    <h2>商品登録フォーム</h2>
+    <p class="lead">下記の入力欄に必要な商品情報を全て入力してください。</p>
+  </div>
+
+  <div class="row">
+    <div class="col-md-12 order-md-1">
+      <h4 class="mb-3">商品の設定</h4>
+      @if (count($errors) > 0)
+        <div class="alert alert-danger">
+          <ul>
+            @foreach ($errors->all() as $error)
+              <li>{{ $error }}</li>
+            @endforeach
+          </ul>
+        </div>
+      @endif
+      <x-alert type="danger" :session="session('danger')"/>
+      <form method="POST" action="{{ route('food.upload') }}" enctype="multipart/form-data">
+        @csrf
+        <div class="row">
+          <div class="col-md-6 mb-3 form-group">
+            <label for="foodName">商品名</label>
+            <input type="string" name="name" class="form-control" id="foodName" placeholder="特からあげ弁当" required autofocus>
+          </div>
+
+          <div class="col-md-6 mb-3 form-group">
+            <label for="price">価格</label>
+            <input type="number" name="price" class="form-control" id="price" min="0" placeholder="540" required>
+          </div>
+        </div>
+
+        <div class="mb-3 form-group">
+          <label for="description">商品の説明</label>
+          <textarea name="description" class="form-control" id="description" placeholder="商品の説明" required></textarea>
+        </div>
+
+        <div class="mt-3 mb-4 form-group">
+          <label for="image">商品画像の設定</label>
+          <input type="file" name="image" class="d-sm-block" id="image" accept="image/png, image/jpeg" required>
+        </div>
+
+        <button class="btn btn-lg btn-primary btn-block mb-5" type="submit">登録する</button>
+      </form>
+    </div>
+  </div>
+</body>
+</html>
--- src/mealbox/resources/views/shared/header.blade.php
+++ src/mealbox/resources/views/shared/header.blade.php
@@ -1,22 +1,14 @@
-<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-  <a class="navbar-brand" href="#">MealBox</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarCollapse">
-    <ul class="navbar-nav ml-auto">
-      @if (Auth::check())
-        <li class="nav-item active">
-          <a class="nav-link" href="/home">Home <span class="sr-only">(current)</span></a>
-        </li>
-      @else
-        <li class="nav-item active">
-          <a class="nav-link" href="/">ログイン <span class="sr-only">(current)</span></a>
-        </li>
-      @endif
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-    </ul>
-  </div>
-</nav>
+<header class="d-flex flex-column flex-md-row fixed-top align-items-center p-3 px-md-4 bg-white border-bottom shadow-sm">
+  <h2 class="my-0 mr-md-auto font-weight-normal">MealBox</h2>
+  <nav class="my-2 my-md-0 mr-md-3">
+    <a class="p-2 text-dark" href="#">マイページ</a>
+  </nav>
+  @if (Auth::check())
+    <form action="{{ route('logout') }}" method="POST" >
+      @csrf
+      <button class="btn btn-outline-primary">ログアウト</button>
+    </form>
+  @else
+    <a class="btn btn-outline-primary" href="/">ログイン</a>
+  @endif
+</header>
