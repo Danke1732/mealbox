0da305a81aa2be509138a03b26df9c21d422913a
Danke1732 <s0lfa1732@gmail.com>
2021-04-12 14:49:25 +0900

    商品一覧表示機能のViewファイル作成

--- src/mealbox/resources/views/admin/admin_top.blade.php
+++ src/mealbox/resources/views/admin/admin_top.blade.php
@@ -6,7 +6,8 @@
 		<div class="card-header">管理側トップページ</div>
 		<div class="card-body d-flex">
 			<div class="mr-2">
-				<a href="{{ route('admin.user_list') }}" class="btn btn-primary">ユーザー一覧</a>
+				<a href="{{ route('admin.user_list') }}" class="btn btn-primary mr-1">ユーザー一覧</a>
+				<a href="{{ route('food.form') }}" class="btn btn-primary">商品投稿</a>
 			</div>
 
 			<form method="post" action="{{ route('admin.logout') }}">
