a1ac3207160a1f7e5166b3af8ae2b73ac7a6a074
Danke1732 <s0lfa1732@gmail.com>
2021-04-15 11:33:39 +0900

    商品削除機能のView作成

--- src/mealbox/resources/views/admin/admin_top.blade.php
+++ src/mealbox/resources/views/admin/admin_top.blade.php
@@ -6,12 +6,12 @@
 		<div class="card-header">管理側トップページ</div>
 		<div class="card-body d-flex">
 			<div class="mr-2">
-				<a href="{{ route('admin.user_list') }}" class="btn btn-primary mr-1">ユーザー一覧</a>
-				<a href="{{ route('admin.food_list') }}" class="btn btn-primary mr-1">商品管理一覧</a>
+				<a href="{{ route('admin.user_list') }}" class="btn btn-primary mb-1 mb-sm-0 mr-1">ユーザー一覧</a>
+				<a href="{{ route('admin.food_list') }}" class="btn btn-primary mb-1 mb-sm-0 mr-1">商品管理一覧</a>
 				<a href="{{ route('food.form') }}" class="btn btn-primary">商品投稿</a>
 			</div>
 
-			<form method="post" action="{{ route('admin.logout') }}">
+			<form method="post" action="{{ route('admin.logout') }}" class="mb-1 mb-sm-0">
 				@csrf
 				<input type="submit" class="btn btn-danger" value="ログアウト" />
 			</form>
