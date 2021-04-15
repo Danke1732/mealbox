cef41e433780c151bf37cf8c1f96dc3ef4c0e43b
Danke1732 <s0lfa1732@gmail.com>
2021-04-15 11:34:34 +0900

    Viewファイル一部修正(表示の仕方)

--- src/mealbox/resources/views/shared/header.blade.php
+++ src/mealbox/resources/views/shared/header.blade.php
@@ -3,9 +3,7 @@
     <h2 class="my-0 mr-md-auto font-weight-normal">MealBox</h2>
     <nav class="my-2 my-md-0 mr-md-3">
       <a class="p-2 text-dark" href="#">マイページ</a>
-      @if (false == session()->get("admin_auth"))
-        <a class="btn btn-outline-primary ml-1" href="{{ route('admin.showLogin') }}">管理者ページ</a>
-      @endif
+      <a class="btn btn-outline-primary ml-1" href="{{ route('admin.showLogin') }}">管理者ページ</a>
       @if (Auth::check())
         <a class="btn btn-outline-primary ml-2" href="{{ route('home') }}">商品一覧</a>
         <form action="{{ route('logout') }}" method="POST" class="d-inline-block ml-1">
