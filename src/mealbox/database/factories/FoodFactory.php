693367c24fa7b4a661d3572148df12811c7e7e28
Danke1732 <s0lfa1732@gmail.com>
2021-04-24 23:03:04 +0900

    各モデルFactory作成

---
+++ src/mealbox/database/factories/FoodFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Food;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class FoodFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Food::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->word,
+            'description' => $this->faker->sentence,
+            'price' => $this->faker->numberBetween($min = 0, $max = 1500),
+            'file_name' => $this->faker->image(),
+            'file_path' => $this->faker->imageUrl(),
+        ];
+    }
+}
